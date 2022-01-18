b59e5b35b2ff30f335b9b4fec9671b570d6ee099
danjhkim <danjhkim@gmail.com>
2022-01-18 02:18:45 -0500

    dffdfd

--- index.js
+++ index.js
@@ -50,13 +50,13 @@ app.use(morgan('combined'));
 // app.use(limiter);
 
 //mongoDB connect
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(result => console.log('connected to db'))
-	.catch(err => console.log(err));
+// mongoose
+// 	.connect(process.env.MONGO_URI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(result => console.log('connected to db'))
+// 	.catch(err => console.log(err));
 
 app.use(cors());
 
